c4887a1707c78e51957cb0c383315c1e6c8c74b7
Afotey Quaye <afotey.quaye@gmail.com>
2016-03-02 00:12:40 -0500

    Homework update

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,8 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Article.create(title: "Article 1", body: "Four score and seven years ago, our fathers encroached on this great land to engage in the wholesale slaughter of the natives. It worked.")
+Article.create(title: "Article 2", body: "Ask not what you can do for your country, but what you can stop doing because of your country.")
+Article.create(title: "Article 3", body: "idc what happens on Super Tuesday, Bernie Sanders will always be number one in our hearts.")
+Article.create(title: "Article 4", body: "I am crappy Don Rickles robot, I've known him almost my whole life.")
+Article.create(title: "Article 5", body: "I am the alien reckoning you awaited. I welcome you Jabba, and I look forward to destroying you.")
